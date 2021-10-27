e0535b867738b3ff68d0e3ec7d14eb8892043337
maryam.bouderham <maryam.bouderham@gmail.com>
2021-10-27 12:36:23 +0000

    crud axios

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 
 const App=()=> {
-  const [data,setData]=useState([])
-  const [error, setError] = useState(null);// where to store the coming errors
- 
-   const fetchData=()=> {// the function to fetch data from the api
-     fetch("https://hn.algolia.com/api/v1/search?query=redux")
-       .then(res => res.json())
-       .then(res => setData(res))
-       .catch(err => setError(err));
-   }
+   const[todos,setTodos]=useState([])
+   const[task,setTask]=useState()
+   const[updatedtask,setUpdatedtask]=useState({})
    useEffect(() => {
-   fetchData();
+   axios.get("https://jsonplaceholder.typicode.com/todos/")
+   .then(res=>setTodos(res.data))
  }, []);
+ const handleSubmit =(e)=>{
+   e.preventDefault()
+   axios.post("https://jsonplaceholder.typicode.com/todos",
+   {
+     userId:1,
+     id:null,
+     title:task,
+     completed:true
+
+   })
+   .then(res=>alert("task"+res.data.id+":added succeffuly"))
+
+ }
+ const handleSubmitEdit =(e)=>{
+  e.preventDefault()
+  axios.put("https://jsonplaceholder.typicode.com/todos/"+updatedtask.id,
+   updatedtask
+
+  )
+  .then(res=>alert("task"+res.data.title+":updated succeffuly"))
+
+}
+ const DeleteTask =(idTask)=>{
+axios.delete(("https://jsonplaceholder.typicode.com/todos/")+idTask)
+.then(res=>alert("task"+idTask+":deleted"))
+ }
+ 
   return (
     <div>
-      hello js
+      <form
+      onSubmit={handleSubmit}
+      className="p-2 text-center">
+        
+        <input type="text" placeholder="title" onChange={(e)=>setTask(e.currentValue)}/>
+        <button type="submit">add task user 1</button>
+      </form>
+      <form
+      onSubmit={handleSubmitEdit}
+      className="p-2 text-center">
+        
+        <input type="text" placeholder="Edit task" onChange={(e)=>setUpdatedtask({...updatedtask,title:e.target.value})}
+        value={updatedtask.title}/>
+        <button type="submit">edit Task</button>
+      </form>
+      <hr />
+      <ul>
+      {todos.map(t=>
+      <li key={t.id}>{t.title}
+      <button onClick={()=>DeleteTask(t.id)}>Delete</button>
+      <button onClick={()=>setUpdatedtask(t)}>Update</button></li>)}
+      </ul>
     </div>
   );
 }
